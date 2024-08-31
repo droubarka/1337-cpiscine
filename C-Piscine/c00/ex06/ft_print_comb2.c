a0506ac099c9fea957b05871f282738292318174
Mohamed A. Oubarka <droubarka@gmail.com>
2024-08-31 15:02:33 -0400

    Update exercise c00/ex06

--- C-Piscine/c00/ex06/ft_print_comb2.c
+++ C-Piscine/c00/ex06/ft_print_comb2.c
@@ -5,15 +5,6 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	write_comb2(short int num1, short int num2)
-{
-	ft_putchar(num1 / 10 + '0');
-	ft_putchar(num1 % 10 + '0');
-	ft_putchar(' ');
-	ft_putchar(num2 / 10 + '0');
-	ft_putchar(num2 % 10 + '0');
-}
-
 void	ft_print_comb2(void)
 {
 	short int	num1;
@@ -25,7 +16,11 @@ void	ft_print_comb2(void)
 		num2 = num1 + 1;
 		while (num2 <= 99)
 		{
-			write_comb2(num1, num2);
+			ft_putchar(num1 / 10 + '0');
+			ft_putchar(num1 % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(num2 / 10 + '0');
+			ft_putchar(num2 % 10 + '0');
 			if (num1 + 1 <= 98)
 				write(1, ", ", 2);
 			num2++;
