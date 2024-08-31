0585a470243b315188c94a24e64dfa44019c4294
Mohamed A. Oubarka <droubarka@gmail.com>
2024-08-31 12:00:27 -0400

    Finished exercise c01/ex08

---
+++ C-Piscine/c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+void	ft_swap(int *a, int *b)
+{
+	if (a != b)
+	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+	}
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	cur_idx;
+	int	nxt_idx;
+	int	min_idx;
+
+	cur_idx = 0;
+	while (cur_idx < size - 1)
+	{
+		min_idx = cur_idx;
+		nxt_idx = cur_idx + 1;
+		while (nxt_idx < size)
+		{
+			if (tab[nxt_idx] < tab[min_idx])
+				min_idx = nxt_idx;
+			nxt_idx++;
+		}
+		ft_swap(&tab[cur_idx], &tab[min_idx]);
+		cur_idx++;
+	}
+}
