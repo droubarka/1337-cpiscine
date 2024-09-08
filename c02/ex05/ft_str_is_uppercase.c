ff5c39ed079fff242b7da17572951a84c0f0e5a8
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-08 18:44:09 -0400

    Update exercise c02/ex05

--- c02/ex05/ft_str_is_uppercase.c
+++ c02/ex05/ft_str_is_uppercase.c
@@ -1,3 +1,4 @@
+/*
 int	ft_str_is_uppercase(char *str)
 {
 	unsigned int	idx;
@@ -8,6 +9,18 @@ int	ft_str_is_uppercase(char *str)
 		if (!('A' <= str[idx] && str[idx] <= 'Z'))
 			return (0);
 		idx++;
+	}
+	return (1);
+}
+*/
+
+int	ft_str_is_uppercase(char *str)
+{
+	while (*str)
+	{
+		if (!('A' <= *str && *str <= 'Z'))
+			return (0);
+		str++;
 	}
 	return (1);
 }
