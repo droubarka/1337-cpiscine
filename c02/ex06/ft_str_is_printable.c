6c9949055277152b9a9979dd0d1f33ec7ca5935c
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-08 18:45:34 -0400

    Update exercise c02/ex06

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -1,3 +1,4 @@
+/*
 int	ft_str_is_printable(char *str)
 {
 	unsigned int	idx;
@@ -8,6 +9,18 @@ int	ft_str_is_printable(char *str)
 		if (!(32 <= str[idx] && str[idx] <= 126))
 			return (0);
 		idx++;
+	}
+	return (1);
+}
+*/
+
+int	ft_str_is_printable(char *str)
+{
+	while (*str)
+	{
+		if (!(32 <= *str && *str <= 126))
+			return (0);
+		str++;
 	}
 	return (1);
 }
