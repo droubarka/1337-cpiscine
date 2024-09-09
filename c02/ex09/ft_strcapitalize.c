9764fe6e85a26a8f7896bcb3cec10a8b16d77489
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-09 19:21:30 -0400

    Update exercise c02/ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -13,6 +13,7 @@ int	is_alnum(char c)
 	return (('0' <= c && c <= '9') || is_uppercase(c) || is_lowercase(c));
 }
 
+/*
 char	*ft_strcapitalize(char *str)
 {
 	unsigned int	is_cap_allowed;
@@ -34,6 +35,32 @@ char	*ft_strcapitalize(char *str)
 		}
 		is_cap_allowed = !is_alnum(str[idx]);
 		idx++;
+	}
+	return (str);
+}
+*/
+
+char	*ft_strcapitalize(char *str)
+{
+	char	is_cap_allowed;
+	char	*buff;
+
+	is_cap_allowed = 1;
+	buff = str;
+	while (*buff)
+	{
+		if (is_cap_allowed)
+		{
+			if (is_lowercase(*buff))
+				*buff -= 32;
+		}
+		else
+		{
+			if (is_uppercase(*buff))
+				*buff += 32;
+		}
+		is_cap_allowed = !is_alnum(*buff);
+		buff++;
 	}
 	return (str);
 }
