eed6f3c63c3818afb440a1cfaad9d63cc2ffc0f5
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-09 20:14:52 -0400

    Update exercise c03/ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,3 +1,4 @@
+/*
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	idx;
@@ -8,6 +9,17 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		if (s1[idx] != s2[idx])
 			return (s1[idx] - s2[idx]);
 		idx++;
+	}
+	return (0);
+}
+*/
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	while (n-- && (*s1 || *s2))
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 	}
 	return (0);
 }
