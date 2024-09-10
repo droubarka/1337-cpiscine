1295202acf556223c4b4b7fa80aa1dadc27c8a20
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-09 21:22:52 -0400

    Update exercise c01/ex03

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -1,14 +1,3 @@
-/*
-void	ft_div_mod(int a, int b, int *div, int *mod)
-{
-	if (b != 0)
-	{
-		*div = a / b;
-		*mod = a % b;
-	}
-}
-*/
-
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
