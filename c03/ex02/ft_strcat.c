25dbdc216d7cfa34b8f06db885240a6c5ff1934d
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-09 20:28:16 -0400

    Update exercise c03/ex02

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -1,3 +1,4 @@
+/*
 unsigned int	ft_strlen(char *str)
 {
 	unsigned int	idx;
@@ -15,6 +16,24 @@ char	*ft_strcat(char *dest, char *src)
 	char	*buff;
 
 	buff = dest + ft_strlen(dest);
+	while (*src)
+	{
+		*buff++ = *src++;
+	}
+	*buff = '\0';
+	return (dest);
+}
+*/
+
+char	*ft_strcat(char *dest, char *src)
+{
+	char	*buff;
+
+	buff = dest;
+	while (*buff)
+	{
+		buff++;
+	}
 	while (*src)
 	{
 		*buff++ = *src++;
