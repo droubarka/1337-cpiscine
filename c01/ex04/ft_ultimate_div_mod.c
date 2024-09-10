f4df9579fb339959ae6bcea62f6b0dbce1710a22
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-09 21:23:13 -0400

    Update exercise c01/ex04

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -1,17 +1,3 @@
-/*
-void	ft_ultimate_div_mod(int *a, int *b)
-{
-	int	rm;
-
-	if (*b != 0)
-	{
-		rm = *a % *b;
-		*a = *a / *b;
-		*b = rm;
-	}
-}
-*/
-
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int	rm;
