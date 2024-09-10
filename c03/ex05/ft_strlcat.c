22b8cbfab8de6aa8a21d2cb7806f539638832e22
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-09 23:26:54 -0400

    Finish exercise c03/ex05

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,29 @@
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	idx;
+
+	idx = 0;
+	while (str[idx])
+	{
+		idx++;
+	}
+	return (idx);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dst_len;
+	unsigned int	src_len;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dest);
+	if (size <= dst_len)
+		return (size + src_len);
+	dest += dst_len;
+	while (1 < size-- - dst_len && *src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+	return (dst_len + src_len);
+}
