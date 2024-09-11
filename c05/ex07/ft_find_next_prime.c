c473d47d074e4f8365d3007ba44bddfccc9920e6
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-11 16:52:41 -0400

    Update exercise c05/ex07

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -40,7 +40,9 @@ int	ft_is_prime(int nb)
 
 int	ft_find_next_prime(int nb)
 {
-	if (nb != 2 && nb % 2 == 0)
+	if (nb <= 2)
+		return (2);
+	if (nb % 2 == 0)
 		nb += 1;
 	while (!ft_is_prime(nb))
 	{
