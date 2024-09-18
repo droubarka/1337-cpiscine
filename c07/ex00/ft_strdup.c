59cace560a881ad1c86d224b15e994820a6197e0
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-17 22:30:39 -0400

    Finish exercise c07/ex00

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,31 @@
+#include <malloc.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	idx;
+
+	idx = 0;
+	while (str[idx])
+	{
+		idx++;
+	}
+	return (idx);
+}
+
+void	ft_strcpy(char *dest, char *src)
+{
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dup;
+
+	dup = malloc(ft_strlen(str) + 1);
+	if (dup != NULL)
+		ft_strcpy(dup, str);
+	return (dup);
+}
